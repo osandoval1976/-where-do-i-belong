b108de133a48686ad16d9a9c78fd9f96e24da398
osandoval1976 <oscarhs0576@gmail.com>
2019-05-09 11:52:32 -0400

    Where do I belong.js
    Concat/sort/indexOF

---
+++ whb.js
@@ -0,0 +1,9 @@
+Where-do-i-belong //First with concat method num to arr //second the sort() method to sort low and high // the IndexOf to grab location of inserted num
+
+function getIndexToIns(arr, num) { // Find my place in this sorted array. let result = arr.concat(num) for(let i=0; i< result.length; i++){ result[i]=result[i]; result.sort(function(a,b){ if(a > b){ return 1; } if(a < b) { return -1; } return 0;
+
+});
+
+} console.log(result); return result.indexOf(num) }
+
+getIndexToIns([40, 60], 50);
